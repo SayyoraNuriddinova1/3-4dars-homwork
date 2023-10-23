eefe6c56e233eb778f3efc3e1494d5b3979a602e
Sayyora <sayyorachalabayeva94@gmail.com>
2023-10-23 22:22:58 +0500

    JSINTRO

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+alert("Bizning so'rovnomamizga xush kelibsiz!")
+confirm("Ma'lumotlaringizni kiritishga tayyormisiz?")
+let fname = prompt("Ismingiz kiriting")
+let lname=prompt("Familyangiz nima?")
+let age=prompt("Tug'ilgan kun, oy va yilingizni kiriting") 
+let adress = prompt ("ro'yxatdan o'tgan manzilngiz")
+let work = prompt ("qaysi sohada faoliyat olib borasiz?")
+let experience = prompt ("nechi yil tajribaga egasiz? (son kiriting)")
+let company = prompt ("qaysi kompaniyalarda ishlagansiz?")
+let goal = prompt ("kelajakdagi maqsadingiz?")
+
+
+console.log ("Men" + " " + fname + " " + lname + " " + age + " " + "tug'ilib," + adress 
++ "dan " + " " + "ro'yxatdan " + " " + "o'tganman." + "Hozirda " + work+ " " + "sohasida "
+ + " " + experience + " " + "yildan"+ " " + "beri" + " " + "faoliyat"+ " " + "olib" + " " + "boraman."+ company + " " 
+ + "ishlab" + " " + "katta"+ " " + "yutuqlarga" + " " + "erishib"+ " " + "kelmoqdaman."  )
+
+
+//Men Nuriddinova Sayyora 11.04.1994 tug'ilib, ...adresdan ro'yxatdan o'tganman va muqaddam sudlanmagan. 
+//dasturlash sohasida 3yildan beri faoliyat olib boradi. ...companiyalarda ishlab katta yutuqlarga erishib kelmoqda.
